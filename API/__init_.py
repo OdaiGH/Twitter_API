6aa0889f3d340852d81179ac916b0a578709bf85
Odai Alghamdi <pr.boy@hotmail.it>
2017-08-16 00:37:20 +0300

    Commit

---
+++ API/__init_.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+__title__ 	 = 'TwitterAPI'
+__author__       = 'Odai Al-Ghamdi'
+__email__        = 'pr.boy@hotmail.it'
+__copyright__    = 'Copyright (c) 2017 The Python-Twitter Developers'
+__url__          = 'https://github.com/OdaiGH/Twitter_API'
+__description__  = 'A python lib for twitter API'
+
+import json
